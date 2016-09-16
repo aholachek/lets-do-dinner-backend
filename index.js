7543299fadb8aa2e589da6ca6070c0d679a88c92
Alex Holachek <aholachek@gmail.com>
2016-09-16 12:54:10 -0400

    latitude longitude issue with yelp api

--- index.js
+++ index.js
@@ -2,7 +2,21 @@
 var rp = require('request-promise');
 rp.debug = true;
 
+var geolib = require('geolib');
+
 var keys = require('./secret_keys');
+
+var locs = {
+  'Davis Square' : { latitude : 42.3967, longitude : -71.1223 },
+  'Union Square' : { latitude : 42.3837, longitude : -71.0958 },
+  'Kendall Square' : { latitude : 42.3629, longitude : 71.0901 },
+}
+
+var center  = geolib.getCenter(locs);
+
+debugger
+
+
 
 var cuisines = [];
 
@@ -42,14 +56,16 @@ function queryYelp(){
     getYelpAccessToken().then(function(){queryYelp(arguments)});
   } else {
 
+    debugger
+
     var options = {
         uri: 'https://api.yelp.com/v3/businesses/search',
         qs : {
           term : 'restaurants',
-          location : '02139',
+          ll : center.latitude + ',' + center.longitude,
           categories : cuisines.join(','),
           limit : 10,
-          price : [1,2,3],
+          price : '1,2,3',
         },
         headers: {
        'Authorization': 'Bearer ' + yelp_access_token
@@ -58,8 +74,11 @@ function queryYelp(){
     };
 
     rp(options)
-        .then(function (repos) {
+        .then(function (response) {
         debugger
+        })
+        .catch(function(error){
+          debugger
         })
 
 
